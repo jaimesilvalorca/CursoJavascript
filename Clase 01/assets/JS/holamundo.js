2a683fdd24dd56e3e52fcfc20bcbc9cf32eb634e
jaimesilvalorca <jaimesilvalorca@gmail.com>
2022-09-08 12:09:56 -0300

    inicial

---
+++ Clase 01/assets/JS/holamundo.js
@@ -0,0 +1,45 @@
+alert("Hola mundo"); /* alerta */
+confirm("eres mayor de edad?"); /* alerta */
+prompt("Ingresa tu edad"); /* ingreso de dato (imput) */
+
+//variables//
+
+let pais = "Chile"; //ya no se usar el var//
+var continente = "America"; //var ya no se usa//
+let antiguedad = 1800;
+//let pais_y_continente = pais+continente; //salida chileamerica//
+let pais_y_continente = `${pais} ${continente}`; //salida chileamerica x+6//
+pais = 'España'; //reaccionacion de la variable//
+continente = 'Europa'; //reaccionacion de la variable//
+
+
+document.write("<h1>Hola mundo desde fichero externo</h1>");
+
+console.log("esto muestra por consola");
+console.log(80+20);
+console.log(pais_y_continente);
+console.log(pais,continente,antiguedad, 'pais y continente', pais_y_continente);
+
+
+var texto1 = "Curso de Javascript";
+console.log(texto1);
+{
+    var texto1 = "Curso de larabel 5";
+    console.log(texto1);
+
+}
+console.log(texto1);
+
+console.log("-------------------------------------------------")
+
+let texto2 = "Curso de Javascript";
+console.log(texto2);
+{
+    let texto2 = "Curso de larabel 5";
+    console.log(texto2);
+
+}
+
+console.log(texto2);
+
+
---
+++ Clase 01/assets/JS/trabajoclases.js
@@ -0,0 +1,13 @@
+let nombre = prompt("Tu nombre");
+let apellido = prompt("Ingresa tu apellido")
+let salida = `${nombre} ${apellido}`;
+alert("Su nombre y apellido es");
+alert(salida);
+
+let numero1 = parseInt(prompt("Numero 1"));
+let numero2 = parseInt(prompt("Numero 2:"));
+let suma = numero1 + numero2
+
+console.log(numero1+numero2);
+
+alert(suma)
