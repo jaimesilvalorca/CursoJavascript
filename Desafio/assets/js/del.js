bb8d4c2357f5c7c8ccbbd4f074fd914ce62e73b4
jaimesilvalorca <jaimesilvalorca@gmail.com>
2022-09-24 04:24:31 -0300

    desafio listo

---
+++ Desafio/assets/js/del.js
@@ -0,0 +1,16 @@
+// alert("tareas asignadas")
+
+// let eliminar = prompt("tienes tareas pendientes, quieres eliminar una?")
+
+// if ((eliminar == 'si')|| (eliminar == 'Si'|| (eliminar == 'SI'))){
+//     alert("se van a eliminar tareas")
+//     pos = parseInt(prompt("Indica el numero que deseas eliminar"))
+//     todo.splice(pos)
+//     console.log(todo)
+    
+//     for( i=0; i < todo.length; i++) {document.write( `<div class="alert alert-success" role="alert">${i+1}.- ${todo[i]}</div>`)}
+
+
+
+    
+// }
---
+++ Desafio/assets/js/todo.js
@@ -0,0 +1,18 @@
+// const todo = []
+// let tamanolist = parseInt(prompt("Ingrese cantidad de lista"));
+// let contador = 0;
+
+// while (contador < tamanolist){
+//     todo.push(prompt("Ingrese tarea a realizar"));
+//     contador++    
+// }
+
+// for( i=0; i < todo.length; i++) {document.write( `<div class="alert alert-success" role="alert">${i+1}.- ${todo[i]}</div>`)}
+
+
+
+
+
+
+
+
